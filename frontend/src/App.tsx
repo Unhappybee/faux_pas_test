c17a4f3205ea88ac8dd75d97a7d3177d381ded44
Nadzeya <xantsi00@vutbr.cz>
2025-05-13 14:36:13 +0200

    fix: delete trace of groupSelect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UserRegistration from "./components/UserRegistration";
-import GroupSelection from "./components/GroupSelection"; // Import new page
 import QuestionnaireForm from "./components/QuestionnaireForm";
 import ResultsPage from "./components/ResultsPage";
 
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<UserRegistration />} />
         <Route path="/register" element={<UserRegistration />} />
-        <Route path="/select-group" element={<GroupSelection />} /> 
         <Route path="/questionnaire" element={<QuestionnaireForm />} />
         <Route path="/results" element={<ResultsPage />} />
       </Routes>
